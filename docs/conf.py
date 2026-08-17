9d5105a7d19ec05b2df8cdbc0cf24e42dd2f59de
Sujan <sujanrajeshb@gmail.com>
2026-08-17 11:16:07 +0100

    Add sphinx project metadata

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+"""Sphinx configuration for tripcrew docs, set up for Read the Docs.
+
+Mirrors the Constellate docs setup (same Sphinx + RTD pattern, same reason:
+get the skeleton right before there's a lot of content to migrate later).
+"""
+
+project = "tripcrew"
+copyright = "2026, Sujan"
+author = "Sujan"
+release = "0.0.1"
